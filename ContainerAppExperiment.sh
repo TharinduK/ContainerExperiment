45c294b8dd434baf60517f31e1b9ef412185de98
TK <tk@bradycorp.com>
2023-12-19 11:41:50 -0600

    add script for update and revision

--- ContainerAppExperiment.sh
+++ ContainerAppExperiment.sh
@@ -31,6 +31,26 @@ az containerapp create \
 
 # Use FQDN retunred to check if container app resource is running 
 
+# Updat container app
+# Update environment variables, compute resources, scale parameters, and deploy a different image
+az containerapp update \
+  -n $caName \
+  -g $rg \
+  --image <IMAGE_NAME>
+
+# list all revisions associated with container app
+az containerapp revision list \
+  -n $caName \
+  -g $rg \
+  -o table
+
+#define secret when creating continer app
+az containerapp create \
+  --resource-group "my-resource-group" \
+  --name queuereader \
+  --environment "my-environment-name" \
+  --image demos/queuereader:v1 \
+  --secrets "queue-connection-string=$CONNECTION_STRING"
 #######################################
 # Clean up resources
 az group delete -n $rg -y
